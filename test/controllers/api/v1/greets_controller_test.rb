5f7b3b3984ebd22562d4847eff3b6a93b8009538
V0pe <dampblew01@gmail.com>
2023-06-07 22:03:15 -0600

    Add the external api to controller

--- test/controllers/api/v1/greets_controller_test.rb
+++ test/controllers/api/v1/greets_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class GreetTest < ActiveSupport::TestCase
+class Api::V1::GreetsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
