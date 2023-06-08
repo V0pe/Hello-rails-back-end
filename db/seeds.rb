13deda1a92ed57b60fd057d62748ef6d4daf528f
V0pe <dampblew01@gmail.com>
2023-06-07 21:52:08 -0600

    Create database and seed some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Greet.create!(text: 'Hi Tom!')
+Greet.create!(text: 'Hi Tamiia!')
+Greet.create!(text: 'Wassup Greg!')
+Greet.create!(text: 'James!')
+Greet.create!(text: 'Hi Vic!')
